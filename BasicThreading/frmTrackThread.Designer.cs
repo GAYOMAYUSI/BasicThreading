9de49ce6f839a4d21bb4775aee0a9d4eacbddaa7
Gayoma <Gayoma@GAYOMAYUSI>
2025-09-29 19:09:52 +0800

    Commit for creating ui design

--- BasicThreading/frmTrackThread.Designer.cs
+++ BasicThreading/frmTrackThread.Designer.cs
@@ -37,22 +37,24 @@
             this.lblMessage.AutoSize = true;
             this.lblMessage.BackColor = System.Drawing.Color.Transparent;
             this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMessage.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblMessage.Location = new System.Drawing.Point(78, 98);
+            this.lblMessage.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblMessage.Location = new System.Drawing.Point(104, 121);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(268, 42);
+            this.lblMessage.Size = new System.Drawing.Size(339, 54);
             this.lblMessage.TabIndex = 0;
             this.lblMessage.Text = "-Thread Starts-";
             // 
             // btnRun
             // 
-            this.btnRun.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnRun.BackColor = System.Drawing.Color.Red;
             this.btnRun.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRun.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnRun.Location = new System.Drawing.Point(131, 179);
+            this.btnRun.Location = new System.Drawing.Point(175, 220);
+            this.btnRun.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRun.Name = "btnRun";
-            this.btnRun.Size = new System.Drawing.Size(137, 52);
+            this.btnRun.Size = new System.Drawing.Size(183, 64);
             this.btnRun.TabIndex = 1;
             this.btnRun.Text = "Run";
             this.btnRun.UseVisualStyleBackColor = false;
@@ -60,13 +62,14 @@
             // 
             // frmTrackThread
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.BackgroundImage = global::BasicThreading.Properties.Resources.beautiful_color_ui_gradients_backgrounds_peach;
-            this.ClientSize = new System.Drawing.Size(395, 321);
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(527, 395);
             this.Controls.Add(this.btnRun);
             this.Controls.Add(this.lblMessage);
+            this.ForeColor = System.Drawing.Color.Yellow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmTrackThread";
             this.Text = "frmTrackThread";
             this.ResumeLayout(false);
--- BasicThreading/frmTrackThread.cs
+++ BasicThreading/frmTrackThread.cs
@@ -20,33 +20,7 @@ namespace BasicThreading
 
         private void btnRun_Click(object sender, EventArgs e)
         {
-            Thread threadA = new Thread(MyThreadClass.Thread1);
-            Thread threadB = new Thread(MyThreadClass.Thread2);
-            Thread threadC = new Thread(MyThreadClass.Thread1);
-            Thread threadD = new Thread(MyThreadClass.Thread2);
-
-            threadA.Name = "Thread A";
-            threadB.Name = "Thread B";
-            threadC.Name = "Thread C";
-            threadD.Name = "Thread D";
-
-            threadA.Priority = ThreadPriority.Highest;
-            threadB.Priority = ThreadPriority.Normal;
-            threadC.Priority = ThreadPriority.AboveNormal;
-            threadD.Priority = ThreadPriority.BelowNormal;
-
-            threadA.Start();
-            threadB.Start();
-            threadC.Start();
-            threadD.Start();
-
-            threadA.Join();
-            threadB.Join();
-            threadC.Join();
-            threadD.Join();
-
-            lblMessage.Text = "-End of Thread-";
-            Console.WriteLine("-End of Thread-");
+        
         }
     }
 }
